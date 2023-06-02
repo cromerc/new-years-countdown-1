953d873b367d26529a3eb72aa9da43c210fbee7a
Chris Cromer <chris@cromer.cl>
2023-06-02 15:44:59 -0400

    finish challenge

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,54 @@
 <script setup>
+import { onMounted, ref } from "vue";
+
 import CountdownHeader from "@/components/CountdownHeader.vue";
 import CountdownSegment from "@/components/CountdownSegment.vue";
+
+const newYears = new Date(new Date().getFullYear() + 1, 0, 1);
+
+const days = ref(0);
+const hours = ref(0);
+const minutes = ref(0);
+const seconds = ref(0);
+
+const updateTimer = () => {
+  const today = new Date();
+
+  // get days
+  let diff = Math.floor((newYears.getTime() - today.getTime()) / 1000);
+  days.value = Math.floor(diff / (3600 * 24));
+
+  // get hours
+  diff -= days.value * 3600 * 24;
+  hours.value = Math.floor(diff / 3600);
+
+  // get minutes
+  diff -= hours.value * 3600;
+  minutes.value = Math.floor(diff / 60);
+
+  // get seconds
+  diff -= minutes.value * 60;
+  seconds.value = Math.floor(diff);
+};
+
+onMounted(() => updateTimer());
+
+setTimeout(() => {
+  setInterval(() => {
+    updateTimer();
+  }, 1000);
+}, 1000 - new Date().getMilliseconds());
 </script>
+
 <template>
   <div class="app-wrapper">
     <div class="countdown-box">
       <CountdownHeader />
       <main class="flex justify-center">
-        <CountdownSegment data-test="days" label="days" number="0" />
-        <CountdownSegment data-test="hours" label="hours" number="0" />
-        <CountdownSegment data-test="minutes" label="minutes" number="0" />
-        <CountdownSegment data-test="seconds" label="seconds" number="0" />
+        <CountdownSegment data-test="days" label="days" :number="days" />
+        <CountdownSegment data-test="hours" label="hours" :number="hours" />
+        <CountdownSegment data-test="minutes" label="minutes" :number="minutes" />
+        <CountdownSegment data-test="seconds" label="seconds" :number="seconds" />
       </main>
     </div>
   </div>
@@ -20,9 +58,11 @@ import CountdownSegment from "@/components/CountdownSegment.vue";
 .app-wrapper {
   @apply flex items-center justify-center w-full h-full p-10;
 }
+
 .countdown-box {
   @apply shadow-md relative bg-white p-5 rounded-lg border-gray-100 border-[1px];
 }
+
 body {
   @apply bg-gray-100;
 }
--- src/components/CountdownSegment.vue
+++ src/components/CountdownSegment.vue
@@ -1,12 +1,15 @@
 <script setup>
 defineProps({
+  number: Number,
   label: String,
 });
 </script>
 <template>
   <div class="segment">
     <div class="number-wrapper">
-      <span class="number">0</span>
+      <Transition>
+        <span class="number" :key="number">{{ number }}</span>
+      </Transition>
     </div>
     <span class="block pt-2 label">{{ label }}</span>
   </div>
@@ -15,31 +18,39 @@ defineProps({
 .segment {
   @apply text-center w-[80px];
 }
+
 .number-wrapper {
   @apply relative pt-10 overflow-hidden;
 }
+
 .number {
   transform: translateX(-50%);
   font-size: 32px;
   @apply absolute top-0 left-[50%];
 }
+
 .label {
   font-size: 16px;
 }
+
 .v-enter-active,
 .v-leave-active {
   transition: all 0.5s ease;
 }
+
 .v-enter-from {
   transform: translateY(-100%) translateX(-50%);
 }
+
 .v-leave-to {
   transform: translateY(100%) translateX(-50%);
 }
+
 .v-enter-to,
 .v-leave-from {
   transform: translateY(0px) translateX(-50%);
 }
+
 .v-enter-from,
 .v-leave-to {
   /* opacity: 0; */
